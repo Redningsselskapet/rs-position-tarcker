60cd5547e59578002a2ab3db365537c1ecd7c33e
Bernt Anker <bernt.anker@me.com>
2019-11-27 20:21:42 +0100

    Fixed issue#1

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -7,7 +7,8 @@ const options = {
   user: DB_USER,
   pass: DB_PASSWORD,
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
 }
 mongoose.Promise = global.Promise
 
