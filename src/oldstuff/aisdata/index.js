8b459bf202c7b216fbc0d4ac205113643b971dd5
Bernt Anker <bernt.anker@me.com>
2019-11-27 20:02:53 +0100

    removed old files

--- src/oldstuff/aisdata/index.js
+++
@@ -1,8 +0,0 @@
-const fetchAisData = require('./fetch-aisdata')
-const aisDataCollector = require('./aisdata-collector')
-const dataMapper = require('../../config/aisdata-provider-service/data-mapper')
-module.exports = {
-  fetchAisData,
-  aisDataCollector,
-  dataMapper
-}
