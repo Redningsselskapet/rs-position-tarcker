8875771660d9687dfa639493259af96fff6eb5b9
Bernt Anker <bernt.anker@me.com>
2019-12-10 15:26:32 +0100

    Faster Moving vessels service

--- src/services/ais-service/get-moving-vessels.js
+++ src/services/ais-service/get-moving-vessels.js
@@ -1,23 +1,7 @@
-const { AisPosition } = require('../../config/database')
-const moment = require('moment')
-const timeWindowInMinutes = 1
-const queryTimeout = parseInt(process.env.QUERY_TIMEOUT)
+const { LastAisPosition } = require('../../config/database')
 
 const getMovingVessels = async () => {
-  const time = moment
-    .utc()
-    .subtract(timeWindowInMinutes, 'minutes')
-    .format('YYYY-MM-DDTHH:mm:ss')
-
-  const positions = await AisPosition.find({
-    Time_stamp: { $gte: time },
-    SOG: { $gte: 1 }
-  }).maxTimeMS(queryTimeout)
-
-  const positionMap = new Map()
-  positions
-    .map(position => position.toObject())
-    .forEach(position => positionMap.set(position.MMSI, position))
-  return [...positionMap.values()]
+  const lastPositions = (await LastAisPosition.find())
+  return lastPositions.map(pos => pos.toObject()).filter(vessel => vessel.SOG > 1)
 }
 module.exports = getMovingVessels
